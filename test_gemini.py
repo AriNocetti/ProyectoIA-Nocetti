fc038a824bc8c644d85959df9470bd195ee2366e
AriNocetti <arinocetti@gmail.com>
2025-03-05 23:35:54 -0300

    borre archivo test

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-GEMINI_API_KEY = "AIzaSyAnApugibmjV4kawBIosyKUO9r6YmLBeyE"
-genai.configure(api_key=GEMINI_API_KEY)
-
-model = genai.GenerativeModel("gemini-1.5-pro")
-
-response = model.generate_content("Hola, ¿puedes responderme?")
-print(response.text)
